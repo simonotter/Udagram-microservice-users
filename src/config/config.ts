331ce6962b99472f22e20dff88c472734cd4791f
Simon Otter <simonotter@gmail.com>
2020-11-17 17:53:52 +0000

    Stripped out Feed endpoint

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,6 @@ export const config = {
   'dialect': 'postgres',
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
   'url': process.env.URL,
   'jwt': {
     'secret': process.env.JWT_SECRET,
